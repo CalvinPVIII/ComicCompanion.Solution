e2268c51ae9a759d0e8cf2a5b221837f9722f564
CalvinPVIII <calvinp003@gmail.com>
2023-11-28 12:43:05 -0800

    WIP reading list and user page, add rating to reading list model, tweak user persistence

--- ComicCompanion/Models/ReadingList.cs
+++ ComicCompanion/Models/ReadingList.cs
@@ -8,4 +8,5 @@ public class ReadingList
     public string UserId { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public int Rating { get; set; }
 }
