2af2f156b61e50e416f5b54335c40619b0e76e00
CalvinPVIII <calvinp003@gmail.com>
2023-12-05 13:03:35 -0800

    add 'add to reading list' button on comic issue page

--- ComicCompanionClient/src/components/ComicIssue.tsx
+++ ComicCompanionClient/src/components/ComicIssue.tsx
@@ -2,9 +2,20 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IIssue } from "../types";
 
+import { useSelector, useDispatch } from "react-redux";
+import { creatingReadingListSelector, currentEditingReadingListSelector } from "../redux/readingListReducer";
+
+import ReadingListHelper from "../helpers/ReadingListHelper";
+
+import "../styles/ComicIssue.css";
+
 export default function ComicIssue() {
   const { comicId, issueId } = useParams();
   const [issue, setIssue] = useState<IIssue | null>();
+
+  const isEditing = useSelector(creatingReadingListSelector);
+  const currentEditingReadingList = useSelector(currentEditingReadingListSelector);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const getInfo = async () => {
@@ -13,8 +24,22 @@ export default function ComicIssue() {
     getInfo();
   });
 
+  const handleAddingToReadingList = (issue: IIssue) => {
+    if (!currentEditingReadingList) return;
+    ReadingListHelper.addIssueToList(issue, currentEditingReadingList, dispatch);
+  };
+
   return (
     <>
+      {isEditing && issue ? (
+        <>
+          <p id="add-to-reading-list" onClick={() => handleAddingToReadingList(issue)}>
+            Add to reading list
+          </p>
+        </>
+      ) : (
+        <></>
+      )}
       <h1>Comic</h1>
       {issue ? (
         <>
