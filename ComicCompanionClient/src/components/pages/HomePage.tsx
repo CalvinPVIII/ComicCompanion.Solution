12ee140ed0f10e2cb67bba726b3c5e0633bea271
CalvinPVIII <calvinp003@gmail.com>
2024-02-22 15:11:15 -0800

    adjust images

--- ComicCompanionClient/src/components/pages/HomePage.tsx
+++ ComicCompanionClient/src/components/pages/HomePage.tsx
@@ -2,38 +2,13 @@ import { Link } from "react-router-dom";
 import "../../styles/HomePage.css";
 import PopularComicsList from "../PopularComicsList";
 import PopularReadingListsList from "../PopularReadingListsList";
-import Slider from "react-slick";
+
 import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
 export default function HomePage() {
-  const sliderOptions = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 15000,
-  };
-  // manually edit photos instead
   return (
     <>
       <h1 id="homepage-header">Comic Companion</h1>
-      <div id="slideshow-wrapper">
-        <div id="img-slideshow">
-          <Slider {...sliderOptions}>
-            {/* <div> */}
-            <img src="https://thecomicvault.files.wordpress.com/2017/01/hush1.jpg?w=1200" />
-            {/* </div> */}
-            <img src="https://oyster.ignimgs.com/wordpress/stg.ign.com/2015/05/rsz_1stk671600.jpg" />
-            <img src="https://i.redd.it/739ohqh54uj41.jpg" />
 
-            <img src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2019/03/Venom-Todd-McFarlane.jpg" />
-
-            <img src="https://cdn.mos.cms.futurecdn.net/8egr5UGFYeCPTSma25o5KX.jpg" />
-            <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/29119f86-bda6-4f27-bbeb-13fdecefb935/df3jrtl-95b63f65-d1f2-4b2b-9702-4e3f93b03551.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI5MTE5Zjg2LWJkYTYtNGYyNy1iYmViLTEzZmRlY2VmYjkzNVwvZGYzanJ0bC05NWI2M2Y2NS1kMWYyLTRiMmItOTcwMi00ZTNmOTNiMDM1NTEuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.QHveQPxLHKRmz7WSMaJU3Tr1aQlpn99b8f3FsSBz2gg" />
-          </Slider>
-        </div>
-      </div>
       <Link to="/comics" className="header-link">
         <h2>POPULAR COMICS</h2>
         <ArrowForwardIcon />
---
+++ ComicCompanionClient/src/helpers/headerImages.ts
@@ -0,0 +1,11 @@
+import justiceLeague1 from "../assets/images/headers/jl1.png";
+import justiceLeague2 from "../assets/images/headers/jl2.png";
+import spiderman1 from "../assets/images/headers/sm1.png";
+import batman1 from "../assets/images/headers/bm1.png";
+import avengers1 from "../assets/images/headers/av1.png";
+import spawn1 from "../assets/images/headers/spw1.png";
+import xmen1 from "../assets/images/headers/xm1.png";
+
+const headerImages = [justiceLeague1, avengers1, batman1, spiderman1, justiceLeague2, spawn1, xmen1];
+
+export default headerImages;
