5de898c19eab671cfdacc23700181ed7c0974c7a
CalvinPVIII <calvinp003@gmail.com>
2024-03-30 10:40:19 -0700

    update patch notes

--- ComicCompanion/Controllers/InfoController.cs
+++ ComicCompanion/Controllers/InfoController.cs
@@ -12,7 +12,7 @@ public class InfoController : Controller
     public ActionResult Get()
     {
         string[] patchNotes = { "Add update checking and patch notes", "Add panning, zoom, and pinch to issue images", "Adjust UI to show which issues have been read" };
-        return Ok(new APIResponseDto("success", 200, new { version = "Beta 1.2", downloadLink = "https://github.com/CalvinPVIII/ComicCompanion.Solution/releases/download/1.0-beta/comiccompanionv1.1-beta.apk", patchNotes }));
+        return Ok(new APIResponseDto("success", 200, new { version = "Beta 1.2", downloadLink = "https://github.com/CalvinPVIII/ComicCompanion.Solution/releases/download/1.2-beta/comiccompanionv1.2-beta.apk", patchNotes }));
     }
 
 
