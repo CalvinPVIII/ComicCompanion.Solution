96607868a7bce07e086038d92d7b1c5a4b07386a
CalvinPVIII <calvinp003@gmail.com>
2024-05-27 11:01:17 -0700

    update in app info

--- ComicCompanion/Controllers/InfoController.cs
+++ ComicCompanion/Controllers/InfoController.cs
@@ -11,8 +11,8 @@ public class InfoController : Controller
     [HttpGet()]
     public ActionResult Get()
     {
-        string[] patchNotes = { "Adjust displaying widescreen images on vertical viewports", "Fix issue with bulk issue select always triggering when scrolling issues" };
-        return Ok(new APIResponseDto("success", 200, new { version = "Beta 1.2", downloadLink = "https://github.com/CalvinPVIII/ComicCompanion.Solution/releases/download/1.2-beta/comiccompanionv1.2-beta.apk", patchNotes }));
+        string[] patchNotes = { "Adjust displaying widescreen images on vertical viewports", "Fix issue with bulk issue select always triggering when scrolling issues", "Adjust case sensitivity when searching for reading lists", "add option to bulk add selected issues to a reading list", };
+        return Ok(new APIResponseDto("success", 200, new { version = "Beta 1.3", downloadLink = "https://github.com/CalvinPVIII/ComicCompanion.Solution/releases/download/1.3-beta/comiccompanionv1.3-beta.apk", patchNotes }));
     }
 
 
